8f434918826ebb9a4a53dca9eb96af6fdc173bc2
allanwelerson <allanwelerson@gmail.com>
2020-12-26 18:04:27 -0300

    Readme atualizado

--- public/js/app.js
+++ public/js/app.js
@@ -66492,8 +66492,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/allan/www/php/tabela/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/allan/www/php/tabela/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/allan/www/php/tournaments_table/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/allan/www/php/tournaments_table/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
