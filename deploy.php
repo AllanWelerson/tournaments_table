f8ab485ba19f65e03d991ae7cca028c3807be8ef
allanwelerson <allanwelerson@gmail.com>
2021-01-03 00:43:50 -0300

    Corrigindo configurações

---
+++ deploy.php
@@ -0,0 +1,40 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Project name
+set('application', 'my_project');
+
+// Project repository
+set('repository', 'https://github.com/AllanWelerson/tournaments_table.git');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true);
+
+// Shared files/dirs between deploys
+add('shared_files', []);
+add('shared_dirs', []);
+
+// Writable dirs by web server
+add('writable_dirs', []);
+
+
+// Hosts
+
+host('root@212.1.214.119')
+    ->set('deploy_path', '~/data/www/');
+
+// Tasks
+
+task('build', function () {
+    run('cd {{release_path}} && build');
+});
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
+// Migrate database before symlink new release.
+
+before('deploy:symlink', 'artisan:migrate');
+
